5a34e409fdffb18f618e610f83d191d7e0e8b1e1
Jacob McCaskey <mccaskey316@gmail.com>
2020-10-16 16:15:32 -0400

    finishes checkoutPage

--- src/components/checkout/Main.js
+++ src/components/checkout/Main.js
@@ -3,6 +3,8 @@ import { User } from "../../Context";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,45 +15,89 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "rgb(240,248,255)",
   },
   thumbnail: {
-    width: "100px",
+    width: "120px",
     height: "auto",
     objectFit: "cover",
     marginLeft: "1rem",
     padding: "1rem",
   },
-  containerCover: {
-    backgroundColor: "rgb(240,248,255)",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    // zIndex: -1000,
-  },
+
   cartContainer: {
-    backgroundColor: "rgb(240,248,255)",
-    width: "100%",
+    width: "60%",
     height: "auto",
-    textAlign: "center",
+    display: "inline-block",
+    marginBottom: "1rem",
+    paddingBottom: "1rem",
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center",
+      width: "100%",
+      display: "block",
+    },
   },
   itemContainer: {
-    borderRadius: "3px 3px 3px 3px",
-    boxShadow: "0px 0px 2px 1px grey",
+    borderRadius: "2px",
+    boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
     backgroundColor: "white",
-    width: "30rem",
+    width: "100%",
+    display: "block",
     textAlign: "left",
-    margin: "1rem",
-    display: "inline-block",
+    // display: "inline-block",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      display: "block",
+    },
+  },
+  shoppingCartHeader: {
+    backgroundColor: "#F8F8F8",
+    borderRadius: "2px",
+    marginBottom: "2rem",
+    padding: "1rem",
+    width: "100%",
+    textAlign: "center",
+    position: "relative",
+    boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+  },
+  dynamicDisplay: {
+    display: "",
+    [theme.breakpoints.down("sm")]: {
+      display: "block",
+    },
+  },
+  checkOutDiv: {
+    float: "right",
+    color: "white",
+    top: 0,
+    backgroundColor: "#002244",
+    marginTop: "5rem",
+    width: "35%",
+    borderRadius: "5px",
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
+      marginTop: "0%",
+      display: "relative",
+      marginBottom: "1rem",
+    },
+  },
+  checkoutBtn: {
+    width: "100%",
+    backgroundColor: "lightgrey",
+    color: "black",
+    borderRadius: "0px 0px 5px 5px",
   },
 }));
 
 export default function Checkout() {
   const styles = useStyles();
   const context = useContext(User);
+  const [subtotal, setSubtotal] = useState(0);
   const { cart } = context;
   useEffect(() => {
-    console.log(cart);
+    let amount = 0;
+    cart.forEach((item) => (amount += item.item.price * item.count));
+    setSubtotal(amount);
   }, [cart]);
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       {cart.length === 0 ? (
         <React.Fragment>
           <Container className={styles.emptyCartContainer}>
@@ -65,19 +111,83 @@ export default function Checkout() {
           </Container>
         </React.Fragment>
       ) : (
-        <div className={styles.cartContainer}>
-          {cart.map((index) => (
-            <Container>
-              <div key={index.item.name} className={styles.itemContainer}>
-                <img
-                  src={index.item.images[0].Location}
-                  className={styles.thumbnail}
-                  alt={index.item.name}
-                />
+        <React.Fragment>
+          <div className={styles.shoppingCartHeader}>
+            <h1>Shopping Cart</h1>
+          </div>
+          <Container>
+            {/* <div className={styles.shoppingCartHeader}>
+            <h1>Shopping Cart</h1>
+          </div> */}
+
+            <Container className={styles.cartContainer}>
+              <h3>
+                {context.firstName}'s Cart ({cart.length} items)
+              </h3>
+              <div className={styles.dynamicDisplay}>
+                {cart.map((index) => (
+                  <div key={index.item.name} className={styles.itemContainer}>
+                    <div style={{ display: "flex" }}>
+                      <div style={{ flex: 1, textAlign: "center" }}>
+                        <img
+                          src={index.item.images[0].Location}
+                          className={styles.thumbnail}
+                          alt={index.item.name}
+                        />
+                        <p>Price: ${index.item.price}</p>
+                      </div>
+                      <div style={{ flex: 2 }}>
+                        <h3>{index.item.name}</h3>
+                        <p>
+                          Size: {index.size} ({index.count})
+                        </p>
+                        <span>
+                          {index.item.quantity > 0
+                            ? "In Stock"
+                            : "Out of Stock"}
+                        </span>
+                        <p>Vendor: {index.item.vendor}</p>
+
+                        <Button onClick={() => context.deleteCart(index)}>
+                          <DeleteIcon />
+                          delete from cart
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </Container>
-          ))}
-        </div>
+            <div className={styles.checkOutDiv}>
+              <Typography variant="h5" style={{ textDecoration: "underline" }}>
+                Summary
+              </Typography>
+              <ul>
+                {context.cart.map((index) => (
+                  <div
+                    style={{
+                      textAlign: "left",
+                    }}
+                    key={index.item.name}
+                  >
+                    <li>
+                      <Typography>
+                        {index.item.name} X {index.count}
+                      </Typography>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+              <Typography variant="h5" style={{ textDecoration: "underline" }}>
+                Subtotal
+              </Typography>
+              <Typography>${subtotal}</Typography>
+              <Button className={styles.checkoutBtn}>
+                <Typography>go to checkout</Typography>
+              </Button>
+            </div>
+          </Container>
+        </React.Fragment>
       )}
     </div>
   );
